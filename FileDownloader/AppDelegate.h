e7f1fc207e561e7b498f0f874558345caca3e59e
nullproduction <nullproduction@gmail.com>
2013-11-05 15:15:56 +0400

    Initial Commit

---
+++ FileDownloader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FileDownloader
+//
+//  Created by mezhevikin on 05.11.13.
+//  Copyright (c) 2013 Mezhevikin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
